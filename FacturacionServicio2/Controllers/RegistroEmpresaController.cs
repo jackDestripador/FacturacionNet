a3ae8eb3bf54676ca55e0fdbbb9eecf1a7b9b468
jesus <chucho.cisneros@hotmail.com>
2021-02-10 14:31:30 -0600

    Creando solucion Facturacion

---
+++ FacturacionServicio2/Controllers/RegistroEmpresaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FacturacionServicio2.Controllers
+{
+    public class RegistroEmpresaController : Controller
+    {
+        // GET: RegistroEmpresa
+        public ActionResult RegistroEmpresa()
+        {
+            return View();
+        }
+    }
+
+     
+}
